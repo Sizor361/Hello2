c1a96322b2e8908bd69f9e8f0c5ca0104467b4ee
Sizor361 <142895529+Sizor361@users.noreply.github.com>
2024-09-04 20:28:26 +0300

    Добавлена система ближнего боя
    Добавил кинжал
    Добавил кулаки

---
+++ HelloWorld.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // «десь исполн€ютс€ команды 1 раз при запуске игры
+    void Start()
+    {
+        print("Hello World!!!");
+    }
+
+    // «десь исполн€ютс€ команды завис€щие от твоего фпс
+    void Update()
+    {
+        
+    }
+}
